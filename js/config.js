2a5bada10c776b5856fb9af0fa704add83f7c93d
hitographic <hito.graphic@gmail.com>
2026-04-03 11:23:01 +0700

    Update Apps Script URL to new deployment with Drive permission

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     
     // Google Sheets Web App URL (akan diisi setelah deploy Apps Script)
     // PENTING: Ganti dengan URL Web App Anda setelah deploy
-    GOOGLE_SHEETS_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbxskbRPzUvJVqEr3TQoLcNvxknfAVlt-JIfcoeYZ-N8OsWegajeUgCxoytuD5yNd_Pdyw/exec',
+    GOOGLE_SHEETS_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbwrngSgjEbkDuNs0B4JWTNzGrih-JkEzjhJac6zmKLdFu2jzsw7YIv30ywwxXBmvO7XMQ/exec',
     
     // Google Drive API Scopes
     // drive.file = only files created by this app (CANNOT read existing files!)
